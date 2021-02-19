77b9cc51148f947fcc86fabe5fa8f91ebd8c866f
Robin van der Linde <robin.vanderlinde@gmail.com>
2021-02-19 15:32:14 +0100

    📝 Added basic documentation to mqtt.simple

--- stubs/umqtt/simple.py
+++ stubs/umqtt/simple.py
@@ -5,38 +5,98 @@ Module: 'umqtt.simple' on esp32 1.14.0
 # Stubber: 1.3.2
 
 class MQTTClient:
-    ''
-    def _recv_len():
+    """The MQTTClient class provides a simple MQTT client.
+    It is designed to be memory efficient, and can be used for subscriptions and publications.
+    https://github.com/micropython/micropython-lib/tree/master/umqtt.simple"""
+
+    def __init__(self, client_id, server, port=0, user=None, password=None, keepalive=0, ssl=False, ssl_params={}):
+        """Initialize an instance of MQTTClient
+
+        Args:
+            client_id: identifier for MQTTClient
+            server: broker to connect to
+            port: port to use for connection, defaults to 0
+            user: username to use for connnection, defaults to empty
+            password: password to use for connection, defaults to empty
+            keepalive: time between keepalive messages, disable using 0, defaults to 0
+            ssl: whether to connect using SSL or not, defaults to False
+            ssl_params: SSL parameters to use with ussl, for more information look at https://docs.micropython.org/en/latest/library/ussl.html
+        """
         pass
 
-    def _send_str():
+    def _recv_len(self):
+        """Returns received length"""
         pass
 
-    def check_msg():
+    def _send_str(self, s: str):
+        """Send string to server
+        
+        Args:
+            s: string to send
+        """
         pass
 
-    def connect():
+    def check_msg(self):
+        pass
+        """Check whether there is a pending message from the sever.
+        If true will work like wait_msg(), else will return immediately"""
+
+    def connect(self, clean_session=True):
+        """Connect to a server
+        Returns True if this connection uses persisten session stored on a server (this will be always False if clean_session=True argument is used (default))."""
         pass
 
-    def disconnect():
+    def disconnect(self):
+        """Disconnect from server"""
         pass
 
-    def ping():
+    def ping(self):
+        """Pings server
+        Message is processed by wait_msg()"""
         pass
 
-    def publish():
+    def publish(self, topic: str, msg: str, retain=False, qos=0):
+        """Publish message
+
+        Args:
+            topic: topic to publish in
+            msg: message to publish
+            retain: whether the broker has to retain the message, defaults to False
+            qos: defines quality of service, defaults to 0
+        """
         pass
 
-    def set_callback():
+    def set_callback(self, f: function):
+        """Set callback function to process messages
+
+        Args:
+            f: function to pass messages to, function is given parameters topic and message
+        """
         pass
 
-    def set_last_will():
+    def set_last_will(self, topic, msg, retain=False, qos=0):
+        """Sets last will message
+        
+        Args:
+            topic: topic to publish in
+            msg: message to publish
+            retain: whether the broker has to retain the message, defaults to False
+            qos: defines quality of service, defaults to 0"""
         pass
 
-    def subscribe():
+    def subscribe(self, topic: str, qos=0):
+        """Subscribe to a topic
+
+        Args:
+            topic: topic to subscribe to
+            qos: defines quality of service, defaults to 0
+        """
         pass
 
-    def wait_msg():
+    def wait_msg(self):
+        """Wait for an incoming message
+        Messages are sent to a callback function, which can be set by using set_callback()
+        """
         pass
 
 
