048c3447cdc67fcd5618f761fcacee526e0ff7c5
wjtje <33957974+wjtje@users.noreply.github.com>
2021-03-18 15:43:40 +0100

    📝 Added ubinascii.hexlify

--- stubs/ubinascii.py
+++ stubs/ubinascii.py
@@ -3,18 +3,26 @@ Module: 'ubinascii' on esp32 1.14.0
 """
 # MCU: (sysname='esp32', nodename='esp32', release='1.14.0', version='v1.14 on 2021-02-02', machine='ESP32 module with ESP32')
 # Stubber: 1.3.2
+
+
 def a2b_base64():
     pass
+
 
 def b2a_base64():
     pass
 
+
 def crc32():
     pass
 
-def hexlify():
-    pass
+
+def hexlify(data: bytes) -> bytes:
+    """Convert the bytes in the data object to a hexadecimal representation. Returns a bytes object.
+
+        If the additional argument sep is supplied it is used as a separator between hexadecimal values.
+    """
+
 
 def unhexlify():
     pass
-
